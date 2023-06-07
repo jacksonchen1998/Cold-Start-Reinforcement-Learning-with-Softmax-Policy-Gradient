016586b2150bae0568baa1be6f3ce4b340120e0b
Jackson <56949000+jacksonchen1998@users.noreply.github.com>
2023-06-07 23:19:25 +0800

    [UPDATE] module

--- model.py
+++ model.py
@@ -112,3 +112,105 @@ class attn(nn.Module):
         c = self.Wa.bmm(hs) # context vector [B, 1, L] @ [B, L, H] = [B, 1, H]
         self.Va = torch.tanh(self.Wc(torch.cat((c, ht), 2)))
         return self.Va # attention vector
+    
+    class Attention(nn.Module):
+    def __init__(self, enc_hid_dim, dec_hid_dim):
+        super().__init__()
+        
+        self.attn = nn.Linear((enc_hid_dim * 2) + dec_hid_dim, dec_hid_dim)
+        self.v = nn.Linear(dec_hid_dim, 1, bias = False)
+        
+    def forward(self, hidden, encoder_outputs):
+        #hidden = [batch size, dec hid dim]
+        #encoder_outputs = [src len, batch size, enc hid dim * 2]
+        
+        batch_size = encoder_outputs.shape[1]
+        src_len = encoder_outputs.shape[0]
+        
+        #repeat decoder hidden state src_len times
+        hidden = hidden.unsqueeze(1).repeat(1, src_len, 1)
+        
+        encoder_outputs = encoder_outputs.permute(1, 0, 2)
+        
+        #hidden = [batch size, src len, dec hid dim]
+        #encoder_outputs = [batch size, src len, enc hid dim * 2]
+        
+        energy = torch.tanh(self.attn(torch.cat((hidden, encoder_outputs), dim = 2))) 
+        
+        #energy = [batch size, src len, dec hid dim]
+
+        attention = self.v(energy).squeeze(2)
+        
+        #attention= [batch size, src len]
+        
+        return F.softmax(attention, dim=1)
+    
+    class Decoder(nn.Module):
+    def __init__(self, output_dim, emb_dim, enc_hid_dim, dec_hid_dim, dropout, attention):
+        super().__init__()
+        
+        self.output_dim = output_dim
+        self.attention = attention
+        self.embedding = nn.Embedding(output_dim, emb_dim)
+        self.rnn = nn.GRU((enc_hid_dim * 2) + emb_dim, dec_hid_dim, num_layers=3, bidirectional = True)
+        self.fc_out = nn.Linear((enc_hid_dim * 2) + dec_hid_dim + emb_dim, output_dim)
+        self.dropout = nn.Dropout(dropout)
+        
+    def forward(self, input, hidden, encoder_outputs):
+             
+        #input = [batch size]
+        #hidden = [batch size, dec hid dim]
+        #encoder_outputs = [src len, batch size, enc hid dim * 2]
+        
+        input = input.unsqueeze(0)
+        
+        #input = [1, batch size]
+        
+        embedded = self.dropout(self.embedding(input))
+        
+        #embedded = [1, batch size, emb dim]
+        
+        a = self.attention(hidden, encoder_outputs)
+                
+        #a = [batch size, src len]
+        
+        a = a.unsqueeze(1)
+        
+        #a = [batch size, 1, src len]
+        
+        encoder_outputs = encoder_outputs.permute(1, 0, 2)
+        
+        #encoder_outputs = [batch size, src len, enc hid dim * 2]
+        
+        weighted = torch.bmm(a, encoder_outputs)
+        
+        #weighted = [batch size, 1, enc hid dim * 2]
+        
+        weighted = weighted.permute(1, 0, 2)
+        
+        #weighted = [1, batch size, enc hid dim * 2]
+        
+        rnn_input = torch.cat((embedded, weighted), dim = 2)
+        
+        #rnn_input = [1, batch size, (enc hid dim * 2) + emb dim]
+            
+        output, hidden = self.rnn(rnn_input, hidden.unsqueeze(0))
+        
+        #output = [seq len, batch size, dec hid dim * n directions]
+        #hidden = [n layers * n directions, batch size, dec hid dim]
+        
+        #seq len, n layers and n directions will always be 1 in this decoder, therefore:
+        #output = [1, batch size, dec hid dim]
+        #hidden = [1, batch size, dec hid dim]
+        #this also means that output == hidden
+        assert (output == hidden).all()
+        
+        embedded = embedded.squeeze(0)
+        output = output.squeeze(0)
+        weighted = weighted.squeeze(0)
+        
+        prediction = self.fc_out(torch.cat((output, weighted, embedded), dim = 1))
+        
+        #prediction = [batch size, output dim]
+        
+        return prediction, hidden.squeeze(0)
