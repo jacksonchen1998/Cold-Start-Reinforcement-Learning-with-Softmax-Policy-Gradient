619897fc661a698b0e5628beff4a6ebf465634c0
kjid1999 <kjid1999@gmail.com>
2023-06-12 16:17:16 +0800

    fix bug

--- inference.py
+++ inference.py
@@ -4,28 +4,28 @@ from torch.nn.functional import softmax
 from torch.distributions import Categorical
 from dataloader import get_vocab, text_transform
 from torchtext.data.utils import get_tokenizer
+import numpy as np
 
 @torch.no_grad()
-def inference(model, ipt, voc):
+def inference(model, ipt, voc, temperature=2):
     '''
     Inference pipeline.
     '''
     model.eval()
     ipt = ipt.to(device)
     output = torch.tensor([voc['<BOS>']], device=device)[None]
-    print(ipt.shape, output)
 
     for t in range(1, len(ipt)+1):
         model_output = model(ipt, output)
         # model_output: (B=1, C), C = voc.size
         top = torch.topk(model_output, k=50, dim=1)
-        prob = top.values
+        prob = softmax(top.values/temperature, dim=1)
         zt_idx = Categorical(probs=prob).sample() # (B,)
-        word_idx = top.indices[zt_idx]
+        word_idx = top.indices[torch.arange(len(top.indices)), zt_idx]
 
         output = torch.cat([output, word_idx[None]], dim=0) # (T, B) token id
     
-    print(voc.get_itos()[output])
+    return np.array(voc.get_itos())[output.cpu()]
 
 if __name__ == '__main__':
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -54,11 +54,12 @@ if __name__ == '__main__':
         device=device
     ).to(device)
 
+    state_dict = torch.load('checkpoint.pth')['model_state_dict']
+    model.load_state_dict(state_dict)
+
     example = '''
-    Reinforcement learning is the study of optimal sequential decision-making in an environment [16]. Its recent developments underpin a large variety of applications related to robotics [11, 5] and games [20].
-    Policy search in reinforcement learning refers to the search for optimal parameters for a given policy parameterization [5]. Policy search based on policy-gradient [26, 21] has been recently applied to
-    structured output prediction for sequence generations.
+    japan 's nec corp. and UNK computer corp. of the united states said wednesday they had agreed to join forces in supercomputer sales .
     '''
-    example = torch.tensor(text_transform(example, SRC_vocab))[:, None]
+    example = torch.tensor(text_transform(example, TRG_vocab))[:, None]
     print(example)
-    inference(model, example, TRG_vocab)
+    ouput_array = inference(model, example, TRG_vocab)
